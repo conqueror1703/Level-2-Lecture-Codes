6b90ba84c2dc398d62295a86ed0e6862018cf425
Aman Singh <conqueror1703@gmail.com>
2025-08-11 11:15:12 +0530

    Update  Class Code 1 _ Prefix Sums.cpp

--- Module 01 - Prefix Sums & Difference Arrays/ Class Code 1 _ Prefix Sums.cpp
+++ Module 01 - Prefix Sums & Difference Arrays/ Class Code 1 _ Prefix Sums.cpp
@@ -1,23 +1,21 @@
-void solve() {
+void solve(){
     int n;
-    cin>>n;
-    vector<int>v(n);
-    for(int i=0;i<n;i++){
-        cin>>v[i];
-    }
-    vector<int>pre(n);
-    pre[0]=v[0];
-    for(int i=1;i<n;i++){
-        pre[i]=pre[i-1]+v[i];
-    }
-    int q;
-    cin>>q;
-    for(int i=0;i<q;i++){
-        int l,r;
-        cin>>l>>r;
-        cout<<pre[r] - ((l==0) ? 0 : pre[l-1])<<" ";
-    }
-    // T.C :- O(n+q)
-    // S.C : -O(n)
-
+cin>>n;
+vector<int>v(n);
+for(int i=0;i<n;i++){//for(auto it:v){
+    cin>>v[i];
 }
+
+vector <int>pre(n);
+pre[0]=v[0];
+for(int i=0;i<n;i++){
+pre[i]=pre[i-1]+v[i];
+}
+
+int main(){
+    int t;
+    cin>>t;
+while(t--){
+int l,r;
+cin>>l>>r>>endl;
+cout<<pre[r]-((l==0)?0 : pre(l-1) ) <<" ";
